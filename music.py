acccd75ff55ec46821f88854951d1bf96f01b58e
farhikhta farzan <farhikhtafarzan@gmail.com>
2023-07-21 20:39:44 -0400

    final files added

--- music.py
+++ music.py
@@ -9,6 +9,8 @@ root.title("musify")
 root.geometry("485x600+290+10")
 root.configure(background="#3F334D")
 root.resizable(False,False)
+mixer.init()
+
 def AddMusic():
     path = filedialog.askdirectory()
     if path:
@@ -16,7 +18,7 @@ def AddMusic():
         songs = os.listdir(path)
         for song in songs:
             if song.endswith(".mp3"):
-                Playlist.insert(END, song)
+                PlayList.insert(END, song)
 def PlayMusic():
     Music = PlayList.get(ACTIVE)
     print(Music(ACTIVE))
@@ -24,14 +26,24 @@ def PlayMusic():
     mixer.music.play()
  
 
-frame = Frame(root, bg="#9E829C", width=485,height=180)
-frame.place(x=0, y= 350)
+frame = Frame(root, bg="#9E829C", width=485,height=150)
+frame.place(x=0, y= 300)
 logo_img = PhotoImage(file="logo.png")
 root.iconphoto(False, logo_img)
 Menu = PhotoImage(file = "menu.png")
-Label(root,image=Menu).place(x=0, y=500,width=485,height=100)
+Label(root,image=Menu).place(x=0, y=450,width=485,height=100)
 frame_music = Frame(root, bd = 2, relief=RIDGE)
-frame_music.place(x= 0, y=550, width = 485, height=100)
-Button(root, text = "Browse Music", width = 60, height = 1, font=("ariel, 12, italic"), fg="Black", bg="Seashell", command=AddMusic).place(x=0, y = 500)
+frame_music.place(x= 0, y=450, width = 485, height=150)
+ButtonPlay = PhotoImage(file="play.png")
+Button(root, image = ButtonPlay, bg = "#FFFFFF", bd=0, height = 50, width = 50, command=PlayMusic).place(x=230,y=387)
+ButtonStop = PhotoImage(file="stop.png")
+Button(root, image = ButtonStop, bg = "#FFFFFF", bd=0, height = 50, width = 50, command=mixer.music.stop).place(x=170,y=387)
+ButtonPause = PhotoImage(file="pause.png")
+Button(root, image = ButtonPause, bg = "#FFFFFF", bd=0, height = 50, width = 50, command=mixer.music.pause).place(x=290,y=387)
+Button(root, text = "Browse Music", width = 60, height = 1, font=("ariel", 12, "italic"), fg="Black", bg="Seashell", command=AddMusic).place(x=0, y = 450)
 Scroll= Scrollbar(frame_music)
+PlayList = Listbox(frame_music, width=100, font = ("ariel", 12),bg = "#3F334D", fg = "white", selectbackground="#9E829C", cursor="arrow", bd=0, yscrollcommand=Scroll.set)
+Scroll.config(command=PlayList.yview)
+Scroll.pack(side=RIGHT, fill= Y)
+PlayList.pack(side=RIGHT, fill = BOTH)
 root.mainloop()
